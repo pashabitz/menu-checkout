020bedb89388de444edf08640a1cdbc2f20abba1
Pasha Bitz <pavelbitz@gmail.com>
2025-02-06 14:29:30 -0800

    Fix reference to local file

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
 export async function GET() {
-    const menu = JSON.parse(fs.readFileSync("data/menu.json", "utf8"));
+    const menu = JSON.parse(fs.readFileSync(`${process.cwd()}/data/menu.json`, "utf8"));
     return new Response(JSON.stringify(menu.categories));
 }

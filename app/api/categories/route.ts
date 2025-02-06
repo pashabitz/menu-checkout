78fb84f907356529ba7382eab950d1943cba0819
Pasha Bitz <pavelbitz@gmail.com>
2025-02-06 14:25:35 -0800

    fix lint errors

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,5 +1,5 @@
-const fs = require("fs");
-export async function GET(request: Request) {
+import fs from "fs";
+export async function GET() {
     const menu = JSON.parse(fs.readFileSync("data/menu.json", "utf8"));
     return new Response(JSON.stringify(menu.categories));
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useQuery } from "react-query";
 
 function Categories() {
@@ -25,6 +24,7 @@ function Categories() {
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="row-start-1 flex gap-6 items-center">Menu Checkout</header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        Hello World
         <Categories />

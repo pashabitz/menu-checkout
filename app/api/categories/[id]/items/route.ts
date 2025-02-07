b58054ebc556643c7842a58742f1165f93cec313
Pasha Bitz <pavelbitz@gmail.com>
2025-02-06 16:07:56 -0800

    Fix route handler param type

--- app/api/categories/[id]/items/route.ts
+++ app/api/categories/[id]/items/route.ts
@@ -1,7 +1,7 @@
 import { getCategoryItems } from "@/lib/menu";
 import { NextRequest } from "next/server";
 
-export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(_: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     const items = getCategoryItems(parseInt(id));
     return new Response(JSON.stringify(items));
